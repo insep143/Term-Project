2f20b77e1d54cb1e4e5652da16fba100cfd49dff
insep143 <dbstndns12@naver.com>
2018-06-15 23:44:35 +0900

    TermProject2

--- term-resume/src/main/java/kr/ac/springboot/term/Question/QuestionController.java
+++ term-resume/src/main/java/kr/ac/springboot/term/Question/QuestionController.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,16 +21,10 @@ public class QuestionController {
 	ResumeRepository resumeRepo;
 	
 	@GetMapping("/QnAregister")
-	public String questionIndex(Model model) {
-		model.addAttribute("result", QRepo.findAllByOrderByNameDesc());
-		return "QnA/QnAregister";
-	}
-	
-	@GetMapping("/Qregister")
     public void registerGET(@ModelAttribute("Q") Question Q) {
     }
 
-    @PostMapping("/Qregister")
+    @PostMapping("/QnAregister")
     public String registerPOST(@ModelAttribute("Q") Question Q) {
     	Optional<Question> q = QRepo.findById(Q.getQnAno());
         if (q.isPresent()) {
